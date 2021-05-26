ac1b0fb59e6a62a96cf2feae8f515297ed720d61
Aykut ÇALIŞKAN <siraykut@gmail.com>
2021-05-26 23:20:23 +0300

    I made some additions.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import {NewsListComponent} from './_components/news-list/news-list.component';
 import {RegisterFormComponent} from './_components/register-form/register-form.component';
 
 const routes: Routes = [
-  {path: 'NewsList', component: NewsListComponent},
+  {path: '',  redirectTo: '/NewsList', pathMatch: 'full'},
+  {path: 'NewsList',  component: NewsListComponent},
   {path: 'News/:Id', component: NewsDetailComponent},
   {path: 'register-form', component: RegisterFormComponent}
 ];
