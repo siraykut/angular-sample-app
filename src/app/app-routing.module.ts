5be6964e298d84db191d4efceebef60e71f31cd6
Aykut ÇALIŞKAN <siraykut@gmail.com>
2021-05-26 23:49:33 +0300

    I made some additions.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {NewsDetailComponent} from './_components/news-detail/news-detail.component';
 import {NewsListComponent} from './_components/news-list/news-list.component';
 import {RegisterFormComponent} from './_components/register-form/register-form.component';
+import {AuthorizationGuard} from './_guards/authorization.guard';
 
 const routes: Routes = [
   {path: '',  redirectTo: '/NewsList', pathMatch: 'full'},
   {path: 'NewsList',  component: NewsListComponent},
   {path: 'News/:Id', component: NewsDetailComponent},
-  {path: 'register-form', component: RegisterFormComponent}
+  {path: 'register-form', component: RegisterFormComponent, canActivate: [AuthorizationGuard] }
 ];
 
 @NgModule({
